ee5a86bb92976183a630be03220c79ef974744d9
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-02-11 00:39:14 +0300

    form UI started

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -8,33 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tarihVerisi = Date()
-    @State private var saatVerisi = Date()
-    let dun = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
-    let yarin = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
+    
     var body: some View {
-        VStack(spacing : 50) {
-            Text("Date picker Örnek - 1")
-                .font(.largeTitle)
-                .foregroundStyle(Color.orange)
-            
-            Text("Datepicker nesnesinden bir değer almak yada değer atamak için değişken atamalıyız.")
-                .frame(maxWidth : .infinity)
-                .padding()
-                .background(Color.red)
-                .foregroundStyle(Color.white)
-                .font(.title)
-            
+        Form {
+            Section {
+                Text("Bu Bir Formdur !!")
+                    .foregroundStyle(.red)
+                    .font(.largeTitle)
+                Text("Birden Fazla nesne alabilir içerisine ekleyebilirsiniz.")
+            }
         }
-        HStack (alignment: .center) {
-                // in parametresine bir closterrange giriyoruz. bitişe bugünü atadığımız için bugün tarih ve saatinde daha ileride birşey seçilemez.
-            DatePicker("", selection: $tarihVerisi , in: dun...yarin , displayedComponents: .date)
-                .environment(\.locale, Locale(identifier: "tr_TR")) // görüntülenecek zamanı türkçe olarak değiştiriyor.
-                .background(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 4)) // bu ayar pickeri sarmalayan bir kenarlık oluşturmaya yarıyor.
-                .padding()
-                
-        }
-        Spacer()
     }
 }
 
