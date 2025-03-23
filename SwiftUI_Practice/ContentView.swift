cd4adbade0cc5d6f7865c4417f172ceeeaea4aca
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-03-24 01:23:15 +0300

    List components started

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -8,33 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var count = ""
+    var basitBirListe = ["Gruplama Özelliği","merhaba","bu oldumu","göreceğiz","bence yaparım"]
     var body: some View {
-        Form {
-            Section(header: Text("Bu inset Metodu").font(.largeTitle)) {
-                Text("herbir satıra istediğimiz gibi girinti yada çıkıntı verebiliriz.")
-                Color.red.listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0)) // bir viewe inset metoduyla kenarlardan ne kadar boşluk bırakması gerektiğini ayarlayabiliriz.
-                Text("herbir satıra istediğimiz gibi girinti yada çıkıntı verebiliriz.")
-                    .listRowInsets(.init(top: 0, leading: 30, bottom: 0, trailing: 0)) // tek başına istediğimiz değerleri girerek sınırları belirleriz
+        List {
+            ForEach(basitBirListe, id: \.self) { item in
+                HStack {
+                    Text(item).font( item == "Gruplama Özelliği" ? .largeTitle : .body)
+                    Spacer()
+                    Image(systemName: item == "bu oldumu" ? "circle.fill" : "circle")
+                }
             }
         }
     }
 }
 
-struct AnotherHeader : View {
-    var name : String
-    var imageName : String
-    
-    var body : some View {
-        HStack {
-            Image(systemName: imageName)
-            Text(name)
-        }
-        .padding(.trailing)
-        .font(.title)
-        .foregroundStyle(.purple)
-    }
-}
+
 
 #Preview {
     ContentView()
