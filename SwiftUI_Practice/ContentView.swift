0b00901d418ecd8832b51a85a366b2645a5017ee
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-03-24 22:55:17 +0300

    add new row in List

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -7,25 +7,52 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State var basitBirListe = ["Gruplama Özelliği","merhaba","bu oldumu","göreceğiz","bence yaparım"] // liste güncelleneceği için state parametresi ekledik
-    var body: some View {
-        List {
-            ForEach(basitBirListe , id: \.self) { veri in
-                Text(veri)
-                    .font(.title2)
-                    
-            }.onDelete(perform: verileriSil) // foreach metoduyla verileri gösterebiliyoruz. bu döngü bittiği yere delete metodunu ekliyoruz ve çağıracak fonksiyonu ayrı olarak tanımlıyoruz.
-        }
-    }
-    // silme işlemi bu fonksiyonda çalışacak silmek istediğimiz indexi alacak ve diziden silecek
-    func verileriSil(at indexler : IndexSet) {
-        if let first = indexler.first {
-            basitBirListe.remove(at: first)
-        }
-    }
+struct IsYapisi : Identifiable {
+    let id = UUID()
+    var isinAdi = ""
+    var isinZamani = ""
+    var girintilimi = false
 }
 
+struct ContentView: View {
+    @State private var isinAdi = ""
+    @State var yapilacakIsler: [IsYapisi] = [
+        IsYapisi(isinAdi : "Yazılım Geliştirici", isinZamani : "10:00", girintilimi : false),
+        IsYapisi(isinAdi : "Ekmek Al", isinZamani : "17:00", girintilimi : false),
+        IsYapisi(isinAdi : "Ders Çalış", isinZamani : "09:00", girintilimi : true),
+        IsYapisi(isinAdi : "Arabayı yıkat", isinZamani : "18:30", girintilimi : false),
+        IsYapisi(isinAdi : "Ödevlerini Bitir", isinZamani : "14:00", girintilimi : true)
+    ]
+    
+    var body: some View {
+        
+        List {
+            Section(header: VStack {
+                Text("Yapılacaklar").font(.largeTitle)
+                HStack {
+                    TextField("yeni işi giriniz", text: $isinAdi)
+                        .textFieldStyle(.roundedBorder)
+                    Button(action: {
+                        yapilacakIsler.append(IsYapisi(isinAdi: isinAdi, isinZamani: "", girintilimi: false))
+                        isinAdi = ""
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .foregroundStyle(.blue)
+                    }
+                }
+            }) {
+                ForEach(yapilacakIsler) { veri in
+                    Text(veri.isinAdi).font(.headline)
+                        .listRowBackground(arkaPlanAyarla(girinti: veri.girintilimi))
+                }
+            }
+        }
+    }
+    
+    func arkaPlanAyarla(girinti : Bool) -> Color {
+        girinti ? Color.green : Color.clear
+    }
+}
 
 
 #Preview {
