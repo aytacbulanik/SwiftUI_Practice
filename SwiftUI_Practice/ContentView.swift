1683d99e276dd9c925e0d1553e9d35a139e9f539
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-03-25 00:34:56 +0300

    navigation view started

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -7,51 +7,27 @@
 
 import SwiftUI
 
-struct IsYapisi : Identifiable {
-    let id = UUID()
-    var isinAdi = ""
-    var isinZamani = ""
-    var girintilimi = false
-}
 
 struct ContentView: View {
-    @State private var isinAdi = ""
-    @State var yapilacakIsler: [IsYapisi] = [
-        IsYapisi(isinAdi : "Yazılım Geliştirici", isinZamani : "10:00", girintilimi : false),
-        IsYapisi(isinAdi : "Ekmek Al", isinZamani : "17:00", girintilimi : false),
-        IsYapisi(isinAdi : "Ders Çalış", isinZamani : "09:00", girintilimi : true),
-        IsYapisi(isinAdi : "Arabayı yıkat", isinZamani : "18:30", girintilimi : false),
-        IsYapisi(isinAdi : "Ödevlerini Bitir", isinZamani : "14:00", girintilimi : true)
-    ]
+   
     
     var body: some View {
-        
-        List {
-            Section(header: VStack {
-                Text("Yapılacaklar").font(.largeTitle)
-                HStack {
-                    TextField("yeni işi giriniz", text: $isinAdi)
-                        .textFieldStyle(.roundedBorder)
-                    Button(action: {
-                        yapilacakIsler.append(IsYapisi(isinAdi: isinAdi, isinZamani: "", girintilimi: false))
-                        isinAdi = ""
-                    }) {
-                        Image(systemName: "plus.circle.fill")
-                            .foregroundStyle(.blue)
-                    }
-                }
-            }) {
-                ForEach(yapilacakIsler) { veri in
-                    Text(veri.isinAdi).font(.headline)
-                        .listRowBackground(arkaPlanAyarla(girinti: veri.girintilimi))
-                }
+        NavigationView {
+            ZStack {
+                Color.blue
+                VStack(spacing: 20) {
+                    Image(systemName: "circle")
+                    Text("Bu ilk metin etiketinin yerleştirildiği yerdir.")
+                }.font(.largeTitle)
+                    .foregroundStyle(.white)
+                Spacer()
             }
+            .navigationBarTitle(Text("Navigation") , displayMode : .inline) //üst tarafta görünecek navigation bara ait metini giriyoruz. display mode ile de boyutunu ayarlıyoruz.
+            
         }
+       
     }
     
-    func arkaPlanAyarla(girinti : Bool) -> Color {
-        girinti ? Color.green : Color.clear
-    }
 }
 
 
