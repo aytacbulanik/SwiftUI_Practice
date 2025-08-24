9dde27ce96b0cd9838ad1bfa240bc2ccd9ad3c70
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-08-24 13:50:03 +0300

    list proteries added

---
+++ Hike.swift
@@ -0,0 +1,8 @@
+//
+//  Hike.swift
+//  SwiftUI_Practice
+//
+//  Created by Aytaç Bulanık on 26.07.2025.
+//
+
+import Foundation
--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -10,22 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
    
-    
+var list = ["deneme","test","olacak mı","görelim"]
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color.blue
-                VStack(spacing: 20) {
-                    Image(systemName: "circle")
-                    Text("Bu ilk metin etiketinin yerleştirildiği yerdir.")
-                }.font(.largeTitle)
-                    .foregroundStyle(.white)
-                Spacer()
-            }
-            .navigationBarTitle(Text("Navigation") , displayMode : .inline) //üst tarafta görünecek navigation bara ait metini giriyoruz. display mode ile de boyutunu ayarlıyoruz.
-            
+        List(list, id: \.self) { item in
+            Text(item)
         }
-       
+            
     }
     
 }
