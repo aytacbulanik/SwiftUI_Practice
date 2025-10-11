4614e3a60838c75b845b646bb7c549f7980b5256
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-10-11 14:23:20 +0300

    navigationview funcs added

--- SwiftUI_Practice/NavigationView_Ornek1.swift
+++ SwiftUI_Practice/NavigationView_Ornek1.swift
@@ -8,30 +8,42 @@
 import SwiftUI
 
 struct NavigationView_Ornek1: View {
+    
+    @State var isOnState : Bool = true
     var body: some View {
         
         NavigationView {
             ZStack {
                 Color.black
+                    .ignoresSafeArea()
                 VStack(spacing: 20) {
-                    Image(systemName: "globe")
-                        .foregroundStyle(.white)
-                        .font(.largeTitle)
-                    Text("Deneme")
-                        .foregroundStyle(Color.green)
-                        .font(.largeTitle)
-                    Text("Navigation view da title eklemezsek aktif olmaz.bu yüzden ilk anda görünmeyebilir")
+                    
+                    Text("NAvigationview ı gizlemek istiyorsanız hidden metodunu kullanmanız gerekir")
                         .frame(maxWidth: .infinity)
                         .padding()
                         .background(Color.blue)
                         .font(.title)
                         .multilineTextAlignment(.center)
                         .foregroundStyle(Color.white)
+                    Toggle(isOn: $isOnState) {
+                        Text(isOnState ? "Açık mı?" : "Kapalı mı?") // yanına yazılacak ifadeyi giriyoruz.
+                            .foregroundStyle(.white)
+                    }
+                    .padding()
+                    
                     Spacer()
                 }
             }
-            .navigationTitle("Navigation View")
-            .navigationBarTitleDisplayMode(.inline) // tek satırlık küçük hale getiriryor.
+            .navigationTitle("Navigation View")//navigation barda yazanı belirliyoruz.
+            .navigationBarTitleDisplayMode(.inline) // tek satırlık küçük hale getiriyor.
+            .navigationBarHidden(isOnState) //bool tipinde bir değer gönderdiğimizde navigation barı açmaya ve kapatmaya yarıyor
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Ekle") {
+                        // action here
+                    }
+                }
+            }// navigation bara buton eklemek için bunu kullanıyoruz. artık toolbar ile gidiliyor. barbuttonitem kaldırılıyor.
         }
     }
 }
