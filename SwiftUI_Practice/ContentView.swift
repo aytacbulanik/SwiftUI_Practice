ef1727d1834ca16d71e51357c16d9d586b36a994
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-10-29 21:43:47 +0300

    segmented cont added and completed

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -10,35 +10,29 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var kullaniciAdi = ""
-    @State private var parola = ""
+    // segmented değerlerini yakalamak için değişkenler oluşturuluyor.
+    @State private var segmentedState = "Gündüz"
+    @State private var segmentedIndex = 1
     
     var body: some View {
         VStack(spacing: 20) {
-            Image(systemName: "circle.fill")
-                .font(.largeTitle)
-                .foregroundStyle(Color.blue)
-                .padding(.bottom,100)
             
-            Text("Secure Field Screen - 1")
-                .font(.largeTitle)
-            Text("Uygulama Login Ekranı")
-                .font(.title)
-                .foregroundStyle(Color.red)
-            // ilk değer place holder da ne yazdığı ile ilgili text değeri ise içine girilen değeri yakalamak için kullanılıyor.
-            TextField("Kullanıcı Adınızı Giriniz", text: $kullaniciAdi)
-                .textFieldStyle(RoundedBorderTextFieldStyle()) // köşeleri yuvarlak border veriyor
+            Text("Segmented Controller Örenk - 1")
+            // text ile picker elemanları oluşturuluyor.
+            Picker("", selection: $segmentedState) {
+                Text("Gündüz").tag("Gündüz")
+                Text("Gece").tag("Gece")
+            }.pickerStyle(.segmented) // bu değeri girince segmented controller a dönüşüyor.
                 .padding()
-            //burada ise şifre olduğu için karakterler gözükmüyor
-            SecureField("Şifreniz",text: $parola)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            HStack {
-                Spacer()
-                Button("Giriş Yap", action: {})
-                
-            }.padding(.trailing,20)
+            Text("Resim ile oluşturulabilir.")
+                .font(.headline)
             
+            // image ile picker oluşturuluyor.
+            Picker("", selection: $segmentedIndex) {
+                Image(systemName: "sun.min").tag(0)
+                Image(systemName: "moon").tag(1)
+            }.pickerStyle(.segmented)
+                .padding(.horizontal)
         }
     }
 }
