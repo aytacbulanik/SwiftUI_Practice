69e1ad7cee8e6a56d6c0feb8eea426ba597634a8
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-12-24 21:36:37 +0300

    text UI completed

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -11,20 +11,23 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        TextView()
+        TextViewAgain()
         
     }
 }
 
-struct TextView : View {
+struct TextViewAgain : View {
+    @State private var isOn = 10.0
     var body: some View {
-        VStack {
-            Text("Merhaba Deneme")
-            Image("image-sun")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding(10)
-                .cornerRadius(50)
+        VStack(spacing : 20) {
+            Text("Bu bir İOS uygulamasıdır.").underline() +
+            Text("SWİFTUI").foregroundStyle(Color.blue)
+                .font(.title)
+                .baselineOffset(4) +
+            Text("Viewlar")
+            
+            Text("Avenir Next")
+                .font(Font.custom("Avenir Next", size: 30))
         }
        
     }
