6d9a58324c741405264fbe9d0d9652decf598540
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-12-28 14:18:48 +0300

    circle completed

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -9,20 +9,29 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
+    @State private var circleProgress : Float = 0.5
+    private var circleCount : Int {
+        Int( circleProgress * 100 )
+    }
     var body: some View {
-        VStack(spacing:10) {
-            Text("Circle")
-                .font(.largeTitle)
-            Circle().padding()
+        VStack {
+            ZStack {
+                Circle().stroke(style: StrokeStyle(lineWidth:15)).fill(Color.init(red: 0, green: 0, blue: 1,opacity: 0.4))
+                    .padding()
+                Circle().trim(from: 0 , to: CGFloat(circleProgress))
+                    .stroke(style: StrokeStyle(lineWidth : 15, lineCap: .round))
+                    .rotationEffect(.degrees(-90)) // bir nesnenin istenilen oranda döndürülmesini sağlar.
+                    .padding()
+                    
+                    
+                
+            }.frame(width: 150, height: 150)
+                .overlay( // overlay metodu nesnenin ortasına başka view koymaya yarıyor.
+                    Text("% \(circleCount)")
+                        .font(.largeTitle)
+                        .foregroundStyle(Color.init(red: 0, green: 0, blue: 1,opacity: 0.4))
+                )
             
-            Text("Capsule")
-                .font(.largeTitle)
-            Capsule().padding()
-            
-            Text("Ellipse")
-                .font(.largeTitle)
-            Ellipse().padding()
         }
         
     }
