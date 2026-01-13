a32ecc6781169183f58a0d52604f493470fc4032
Aytac Bulanik <aytacbulanik@hotmail.com>
2026-01-13 23:20:28 +0300

    added a lot of pickers

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -13,21 +13,33 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercent = 20
     
-    let tipArray = [0,10,25,20,25]
+    let tipArray = [0,10,25,20,15]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Hesap tutarını giriniz", value: $billAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad) // veriyi double olarak alıyoruz ve telefonun varsayılan para birimi olarak yazdırıyoruz.
-                Picker("Select People" , selection: $numberOfPeople) {
-                    ForEach(2..<100) {
-                        Text("\($0) people")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Hesap tutarını giriniz", value: $billAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad) // veriyi double olarak alıyoruz ve telefonun varsayılan para birimi olarak yazdırıyoruz.
+                    Picker("Select People" , selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
                     }
+                    .pickerStyle(.navigationLink)
+                }
+                Section ("How much do you want to tip ?"){
+                    Picker("Select Percentes" , selection: $tipPercent) {
+                        ForEach(tipArray , id: \.self) {
+                            Text("% \($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section {
+                    Text(billAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
-            Section {
-                Text(billAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-            }
+            .navigationTitle("View Split")
         }
     }
 }
