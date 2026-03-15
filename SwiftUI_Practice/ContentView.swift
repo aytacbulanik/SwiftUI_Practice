5ceb2d8f71595e6e89388aa7ede566dc9084954c
Aytac Bulanik <aytacbulanik@hotmail.com>
2026-03-16 00:53:06 +0300

    use another struct

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -7,18 +7,26 @@
 
 import SwiftUI
 
+struct CapsuleText : View {
+    var text : String
+    
+    var body: some View {
+        Text(text)
+            .font(Font.largeTitle)
+            .padding()
+            .background(.blue)
+            .foregroundStyle(.white)
+            .clipShape(.capsule)
+    }
+}
+
 
 struct ContentView: View {
     @State private var textColor = false
     
     var body: some View {
-        Button("Hello, World") {
-            textColor.toggle()
-        }
-        .frame(width: 100,height: 100)
-        .padding()
-        .background(textColor ? .blue : .red)
-        .foregroundStyle(textColor ? .red : .blue)
+        CapsuleText(text: "First")
+        CapsuleText(text: "Second")
         
     }
     
