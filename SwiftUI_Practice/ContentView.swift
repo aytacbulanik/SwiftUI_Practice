3fd188cb0f855fd986b98bba68066ec737f04879
Aytac Bulanik <aytacbulanik@hotmail.com>
2026-03-24 22:13:32 +0300

    small game completed

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -7,30 +7,73 @@
 
 import SwiftUI
 
-struct CapsuleText : View {
-    var text : String
-    
-    var body: some View {
-        Text(text)
-            .font(Font.largeTitle)
-            .padding()
-            .background(.blue)
-            .foregroundStyle(.white)
-            .clipShape(.capsule)
-    }
-}
 
 
 struct ContentView: View {
-    @State private var textColor = false
+    
+    var objects = ["Rock" , "Scissors" , "Paper"]
+    @State var randomNumber = Int.random(in: 0..<3)
+    @State private var chose = ""
+    @State private var P1Score = 0
+    @State private var state = false
     
     var body: some View {
-        CapsuleText(text: "First")
-        CapsuleText(text: "Second")
+       Text("Welcome to the game")
+            .font(.largeTitle)
+            .foregroundStyle(.orange)
+        HStack (spacing : 20) {
+            Text("Your Score: \(P1Score)")
+                .font(Font.largeTitle.bold())
+            
+        }
         
+        ForEach(objects , id: \.self) { choise in
+            Button(choise) {
+                switch objects[self.randomNumber] {
+                case "Rock" :
+                    if choise == "Paper" {
+                        self.state = true
+                        self.P1Score += 1
+                    } else {
+                        self.state = false
+                    }
+                case "Scissors" :
+                    if choise == "Rock" {
+                        self.state = true
+                        self.P1Score += 1
+                    }else {
+                        self.state = false
+                    }
+                case "Paper" :
+                    if choise == "Scissors" {
+                        self.state = true
+                        self.P1Score += 1
+                    }else {
+                        self.state = false
+                    }
+                default:
+                    break
+                }
+                randomNumber = Int.random(in: 0..<3)
+            }
+            .frame(width: 200 , height: 50)
+            .padding()
+            .background(.blue)
+            .cornerRadius(14)
+            .foregroundStyle(.white)
+        }
+        Text(objects[self.randomNumber])
+            .foregroundStyle(.orange)
+            .font(.system(size: 40))
+            .padding()
+        Text(state ? "DOĞRU" : "YANLIŞ")
+            .padding()
+            .background(state ? .green : .red)
+            .foregroundStyle(.white)
+            .font(.title)
+            
+        Spacer()
     }
-    
-    
 }
 
 
